85ec5ff53fa443160bd008cc9e39595c3cd65541
natedaly <nate@natedaly.net>
2016-03-23 19:45:13 -0400

    Publish user data to client
    To check if a player exists or not during login we need to be able to
    query the users collection, so this change publishes it to the client.

--- server/publications.js
+++ server/publications.js
@@ -1,10 +1,5 @@
-Meteor.publish("userData", function () {
-    if (this.userId) {
-        return Meteor.users.find({_id: this.userId},
-                                 {fields: {'inCombat': 1}});
-    } else {
-        this.ready();
-    }
+Meteor.publish('userData', function () {
+  return Meteor.users.find({}, { username: 1, inCombat: 1 });
 });
 
 Meteor.publish("rooms", function() {
